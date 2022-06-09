6cd84de9a68f97e362bf5832662690aa376b8105
SohamDas04 <21051942@kiit.ac.in>
2022-06-09 15:31:12 +0530

    Changed Jquery

--- resources/views/myaccount.blade.php
+++ resources/views/myaccount.blade.php
@@ -14,10 +14,10 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <!-- <link rel="stylesheet" href="https://unpkg.com/@popperjs/core@2"> -->
   <link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css">
-  <script src="/JS/jQuery.js"></script>
+  <!-- <script src="/JS/jQuery.js"></script> -->
   <!-- <script src="/JS/bootstrap.js"></script> -->
   <!-- <script src="/JS/popper.js"></script> -->
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
   <style>
@@ -308,7 +308,7 @@
     //  fd.append('_token',CSRF_TOKEN);
     // console.log(fd);
     // AJAX request 
-    $.post({
+    $.ajax({
       url: "/uploadp",
       method: 'post',
       data: fd,
@@ -341,7 +341,7 @@
     //  fd.append('_token',CSRF_TOKEN);
     console.log(fd);
     // AJAX request 
-    $.post({
+    $.ajax({
       url: "/uploacp",
       method: 'post',
       data: fd,
